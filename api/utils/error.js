c068e2e2a5d8673953fcbc74f824871d330d3c47
prachidimote <81797753+prachidimote@users.noreply.github.com>
2024-03-08 17:41:00 +0530

    create signin api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,7 @@
 //custom function for manual error handler by user
 export const errorHandler = (statusCode, message) => {
     const error = new Error();
+    error.statusCode = statusCode;
     error.message = message;
     return error;
-}
+};
